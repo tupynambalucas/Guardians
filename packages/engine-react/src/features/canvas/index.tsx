52b8133ab14572913e28dcd64391e6b63288d090
Tupynambá Lucas <tupynambalucas@gmail.com>
2026-03-03 08:19:35 -0300

    Canvas: Enviroment background enabled / intensity changes

--- packages/engine-react/src/features/canvas/index.tsx
+++ packages/engine-react/src/features/canvas/index.tsx
@@ -16,7 +16,7 @@ function SceneCanvas() {
     renderer.shadowMap.enabled = true;
     renderer.shadowMap.type = THREE.PCFSoftShadowMap;
     renderer.toneMapping = THREE.NeutralToneMapping;
-    renderer.toneMappingExposure = 0.8;
+    renderer.toneMappingExposure = 0.5;
     renderer.setPixelRatio(window.devicePixelRatio);
     await renderer.init();
     return renderer as unknown as THREE.Renderer;
@@ -24,7 +24,15 @@ function SceneCanvas() {
 
   return (
     <Canvas gl={glConfig} shadows camera={{ position: [0, 0, 15], fov: 25 }}>
-      <Environment files={nightsky as string} backgroundBlurriness={0.8} />
+      <Environment
+        files={nightsky as string}
+        background={true}
+        preset={undefined}
+        backgroundBlurriness={0} // optional blur factor between 0 and 1 (default: 0, only works with three 0.146 and up)
+        backgroundIntensity={0.8} // optional intensity factor (default: 1, only works with three 0.163 and up)
+        backgroundRotation={[0, Math.PI / 2, 0]} // optional rotation (default: 0, only works with three 0.163 and up)
+        environmentIntensity={0.5}
+      />
 
       <ambientLight intensity={0.5} />
       <directionalLight position={[10, 20, 15]} intensity={1.5} castShadow shadow-bias={-0.0001} />
